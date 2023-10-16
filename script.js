deab6f1dfefc803ede233ecc72ec48cae0e6b4a0
Aiste <icy.aiste@gmail.com>
2023-10-16 12:40:38 +0200

    added 4 & 5

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ document.getElementById("title").innerText = "Potato";
 document.getElementById('start').innerHTML = "Start";
 
 document.getElementById('mailus').innerHTML = "Mail Us";
+
+document.getElementById('hoodie').innerText = "This hoodie is dope. Definitely worth buying!";
+
+document.getElementById('btn').style.backgroundColor = "forestgreen";
+
