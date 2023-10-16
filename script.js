05d4232c5a5d53befec3c4a2ae3b2edac32e4665
Aiste <icy.aiste@gmail.com>
2023-10-16 13:20:31 +0200

    added up to 9th

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ document.getElementById('hoodie').innerText = "This hoodie is dope. Definitely w
 
 document.getElementById('btn').style.backgroundColor = "forestgreen";
 
+document.getElementById('poster').style.backgroundColor = "lightblue";
+
+document.getElementById('address').innerText = 'Stockholmsvägen 99';
+
+let main = document.querySelectorAll('p');
+for (let i = 0; i < main.length; i++) {
+  main[i].style.color = "darkgreen";
+}
+
+let btn = document.querySelectorAll('button');
+for (let i = 0; i < btn.length; i++) {
+  btn[i].innerHTML = "Add to cart";
+}
