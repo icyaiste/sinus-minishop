88abf9d4cc4700739e1959b47c823933f8675b54
Aiste <icy.aiste@gmail.com>
2023-10-16 12:31:53 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Code here! 
+
+*/
+
+document.getElementById("title").innerText = "Potato";
+
+document.getElementById('start').innerHTML = "Start";
+
+document.getElementById('mailus').innerHTML = "Mail Us";
